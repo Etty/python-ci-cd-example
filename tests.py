721b96a5268539ec4f3cf77af0a2e275c785076a
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2025-09-22 18:24:36 +0200

    update tests and ignore cache

--- tests.py
+++ tests.py
@@ -1,11 +1,27 @@
 import unittest
 import app
 
+
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
         import app
-        self.assertEqual(app.hello_world(), "Hello, World!")
 
-if __name__ == '__main__':
+        self.assertEqual(
+            app.hello_world(),
+            """Hello, I am Anastasiia Yermolik!<br> 
+    Yes, I'm an experienced Magento developer, 
+    but now I'm looking for new oppoptunities and discovering another directions.<br>    
+    This sample project demonstrates my knowledge of CI/CD technology and Python fundamentals.<br>
+    Also I learn Java and gained OCA certification<br>
+    Also I am a beginner Blockchain developer<br>
+    And also continue learning AI-driven development<br>
+    Please follow my 
+    <a href='https://www.linkedin.com/in/anastasiia-yermolik-b39140b9/' target='_blank'>
+    LinkedIn profile</a>
+    for more info""",
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
